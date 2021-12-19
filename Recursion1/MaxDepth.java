16ce9e862cc67577ccabc7cc95c93505a2ff9b4c
Jacob Harrington <jharring@usc.edu>
2021-12-19 12:50:18 -0800

    deepest path given a binary search tree.

--- Recursion1/MaxDepth.java
+++ Recursion1/MaxDepth.java
@@ -1,7 +1,5 @@
-import java.util.HashMap;
 public class MaxDepth 
-    {
-
+{
     //  Definition for a binary tree node.
     public class TreeNode 
     {
@@ -18,27 +16,20 @@ public class MaxDepth
         }
     }
     class Solution {
-        HashMap<Integer,Integer> memo;
         public int depthHelp(TreeNode node, int count)
         {
+            // non-existent branch
             if(node == null)
                 return count - 1;
             
-            // if(memo.containsKey(node.val))
-            //     return memo.get(node.val);
-            
-            memo.putIfAbsent(node.val,
-                Math.max(
+            // look left/right for longer path to bottom from node
+            return Math.max(
                     depthHelp(node.left, count+1),
                     depthHelp(node.right, count+1)
-                )
-            );
-            return memo.get(node.val);
+                    );
         }
         public int maxDepth(TreeNode root) 
         {
-            // <key = node.val,  value = max depth to leaf> 
-            memo = new HashMap<Integer,Integer>();
             return depthHelp(root,1);
         }
     }
