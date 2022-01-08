67fa8f6c9260744e5e656a81c872545b94270ac2
Jacob Harrington <jharring@usc.edu>
2022-01-07 16:39:42 -0800

    reformat & added prompt

--- Interview_Questions/BestTimeToBuySellStockII.py
+++ Interview_Questions/BestTimeToBuySellStockII.py
@@ -1,18 +1,32 @@
+# You are given an integer array prices where prices[i] is the price of a given stock on
+# the ith day. On each day, you may decide to buy and/or sell the stock. You can only 
+# hold at most one share of the stock at any time. However, you can buy it then 
+# immediately sell it on the same day. Find and return the maximum profit you can achieve.
 
-# def help(prices: List[int], memo: List[int], day):
-#     # base case, outside date range
-#     if day >= len(prices):
-#         return 0
-#     elif day == len(prices)-1:
-#         return prices[day]
-#     # if precomputed
-#     elif memo[day] != -1:
-#         return memo[day]
+# Example 1:
+# Input: prices = [7,1,5,3,6,4]
+# Output: 7
+# Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+# Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+# Total profit is 4 + 3 = 7.
 
-#     memo[day] = max(0, help(prices, memo, day+1) - prices[day],
-#                          )
+# Example 2:
+# Input: prices = [1,2,3,4,5]
+# Output: 4
+# Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+# Total profit is 4.
+
+# Example 3:
+# Input: prices = [7,6,4,3,1]
+# Output: 0
+# Explanation: There is no way to make a positive profit, so we never buy the stock to 
+# achieve the maximum profit of 0.
+ 
+# Constraints:
+# 1 <= prices.length <= 3 * 104
+# 0 <= prices[i] <= 104
+
 from typing import List
-
 
 def maxProfit(prices: List[int]) -> int:
     total = 0
@@ -22,14 +36,15 @@ def maxProfit(prices: List[int]) -> int:
     return total
 
 
-l = list(range(0, 100))
-print(maxProfit(l))
-print(maxProfit([1, 1, 2, 0, 1]))
-print(maxProfit([1, 6, 2, 4]))
-print(maxProfit([7, 1, 5, 3, 6, 4]))
-print(maxProfit([1, 1, 1, 2]))
-print(maxProfit([1, 2, 3, 4, 5]))
-print(maxProfit([7, 6, 5]))
+## TEST CASES 
+# l = list(range(0, 100))
+# print(maxProfit(l))
+# print(maxProfit([1, 1, 2, 0, 1]))
+# print(maxProfit([1, 6, 2, 4]))
+# print(maxProfit([7, 1, 5, 3, 6, 4]))
+# print(maxProfit([1, 1, 1, 2]))
+# print(maxProfit([1, 2, 3, 4, 5]))
+# print(maxProfit([7, 6, 5]))
 
 # Recursive solution, needs memoization to speed up for larger lengths
 ######################################################################
