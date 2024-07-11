de816741786593a58aa699166767148cdee87285
Jacob Harrington <jharring@usc.edu>
2024-07-10 22:38:31 -0700

    Including problem definition

--- Graphs/BreadthFirstSearch/rottingOranges.py
+++ Graphs/BreadthFirstSearch/rottingOranges.py
@@ -1,3 +1,33 @@
+'''
+You are given an m x n grid where each cell can have one of three values:
+    0 representing an empty cell,
+    1 representing a fresh orange, or
+    2 representing a rotten orange.
+Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+Return the minimum number of minutes that must elapse until no cell has a fresh orange. 
+If this is impossible, return -1.
+
+Example 1:
+Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+Output: 4
+
+Example 2:
+Input: grid = [[2,1,1],[0,1,1],[1,0,1]]
+Output: -1
+Explanation: The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+
+Example 3:
+Input: grid = [[0,2]]
+Output: 0
+Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
+
+Constraints:
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 10
+grid[i][j] is 0, 1, or 2.
+'''
+
 from typing import List
 
 class BFS:
@@ -8,12 +38,15 @@ class BFS:
         self.goodOranges = []
         self.maxTimeToRot = 0
 
+    # We will be BFS traversing from all good oranges to the nearest rotten orange
+    # We don't need to BFS traverse for empty squares or rotten squares
     def findAllGoodOranges(self) -> None:
         for i in range(self.m):
             for j in range(self.n):
                 if self.grid[i][j] == 1:
                     self.goodOranges.append([i,j])
 
+    # Add valid steps away from a given coordinate to a list result
     def getAllValidSteps(self, coordinate: List[int], visited: List[List[int]]) -> List[List[int]]:
         steps = []
         if (coordinate[0] > 0 and                               # can step left
@@ -34,12 +67,18 @@ class BFS:
             steps.append([coordinate[0],coordinate[1]+1])
         return steps
 
+    # BFS traverse from a good orange to the nearest rotten orange
+    # Return false if no rotten connected
     def traverseToRottenOrange (self, coordinate: List[int]) -> bool:
+
+        # Don't reevaluate squares we've already visited
         visited = [[0 for _ in range(self.n)] for _ in range(self.m)]
 
+        # Traverse grid until we find a rotten orange or have evaluated all connected squares
         q = self.getAllValidSteps(coordinate, visited)
         step = 0
         while len(q) > 0:
+            # Helps track the level of traversal, or steps we take towards a rotten orange
             size = len(q)
             step += 1
             for _ in range(size):
@@ -50,11 +89,10 @@ class BFS:
                         self.maxTimeToRot = step
                     return True
                 q += self.getAllValidSteps(neighbor, visited)
-        
+
+        # No rotten orange connected to a healthy orange
         return False
-
-
-
+    
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         bfs = BFS(grid)
@@ -62,9 +100,7 @@ class Solution:
         for goodOrange in bfs.goodOranges:
             if bfs.traverseToRottenOrange([goodOrange[0], goodOrange[1]]) == False:
                 return -1
-            
         return bfs.maxTimeToRot
-    
 
 s = Solution()
 print (s.orangesRotting([[0,1],[0,0],[2,0]])) # -1
