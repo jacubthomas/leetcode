7df92e7e014395de79027bc3a117c0dc9f44f671
Jacob Harrington <jharring@usc.edu>
2024-07-28 13:08:37 -0600

    Refreshing on python3 Pandas

---
+++ Pandas/createADataFrameFromList.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from typing import List
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    df = pd.DataFrame.from_records(student_data)
+    # df = df.transpose()
+    df.columns = ["student_id", "age"]
+    return df
+
+student_data = [[1,15],[2,11],[3,11],[4,20]]
+print (createDataframe(student_data))
